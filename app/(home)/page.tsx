1b7eaf49c46abbe84a4aaee847f3a5e6f7e43b05
Nirmal <nirmalmhjr@users.noreply.github.com>
2025-11-22 14:02:33 +0545

    feat: create website Layout

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default async function Page() {
+  return (
+    <div>
+      <h1 className='h1-bold text-center p-10'>Home Page Content</h1>
+    </div>
+  )
+}
+
---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,23 @@
+import {ShoppingCartIcon, UserIcon} from 'lucide-react'
+import Link from 'next/link'
+
+export default function Menu(){
+
+    return(
+        <div className='flex justify-end'>
+            <nav className='flex gap-3 w-full'>
+                <Link href={"/signin"} className='header-button flex items-center'>
+                    <UserIcon className='size-8'/>
+                    <span className='font-bold'>Hello, Sign In</span>
+                </Link>
+
+                <Link href={"/cart"} className='header-button'>
+                    <div className='flex items-end'>
+                        <ShoppingCartIcon className='size-8'/>
+                        <span className='font-bold'>Cart</span>
+                    </div>
+                </Link>
+            </nav>
+        </div>
+    )
+}
