7941920adc16eb7094311a710adcb8294cf4e369
Nirmal <nirmalmhjr@users.noreply.github.com>
2025-11-24 11:17:31 +0545

    feat: create home-page-carousel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,12 @@
+import { HomeCarousel } from '@/components/shared/home/home-carousel'
 import React from 'react'
+import data from '@/lib/data'
 
 export default async function Page() {
   return (
     <div>
-      <h1 className='h1-bold text-center p-10'>Home Page Content</h1>
+      {/* <h1 className='h1-bold text-center p-10'>Home Page Content</h1> */}
+      <HomeCarousel items={data.carousels}/>
     </div>
   )
 }
