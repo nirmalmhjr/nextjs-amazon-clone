052308aa82658bb0f00051fc87a157b8055d13f7
Nirmal <nirmalmhjr@users.noreply.github.com>
2026-01-24 08:57:30 +0545

    feat: pay-order-by-stripe

--- app/checkout/[id]/page.tsx
+++ app/checkout/[id]/page.tsx
@@ -4,6 +4,8 @@ import { getOrderById } from "@/lib/actions/order.actions"
 import { formatId } from "@/lib/utils"
 import Link from "next/link"
 import { notFound } from "next/navigation"
+import Stripe from 'stripe'
+import PaymentForm from "./payment-form"
 
 export default async function  CheckoutPaymentPage (props: {
     params: Promise<{id: string}>
@@ -17,9 +19,27 @@ export default async function  CheckoutPaymentPage (props: {
 
     const session = await auth()
 
+    let client_secret = null
+if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: Math.round(order.totalPrice * 100),
+    currency: 'USD',
+    metadata: { orderId: order._id.toString() },
+  })
+  client_secret = paymentIntent.client_secret
+}
+// client_secret = { client_secret }
+
     return (
         <>
-            <div className="flex gap-2">
+        <PaymentForm 
+            order={order}
+            paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+            clientSecret = {client_secret}
+            isAdmin={session?.user?.role === 'Admin' || false}
+        />
+            {/* <div className="flex gap-2">
                 <Link href={'/account'}>Your Account</Link>
                 <span>{'>'}</span>
                 <Link href={'/account/orders'}>Your Orders</Link>
@@ -30,7 +50,8 @@ export default async function  CheckoutPaymentPage (props: {
             <OrderDetailsForm 
                 order={order}
                 isAdmin={session?.user.role === 'Admin' || false}
-            />
+            /> */}
+            
         </>
     )
 
