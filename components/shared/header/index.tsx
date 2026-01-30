6dce90e359b393201b544af41b7374625b7f54dc
Nirmal <nirmalmhjr@users.noreply.github.com>
2026-01-30 22:08:36 +0545

    feat: create-category-sidebar

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -7,8 +7,13 @@ import data from "@/lib/data"
 import Link from "next/link"
 import Image from "next/image"
 import { APP_NAME } from "@/lib/constants"
+import Sidebar from "./sidebar"
+import { getAllCategories } from "@/lib/actions/product.actions"
 
-function Header() {
+
+export default async function Header() {
+    const categories = await getAllCategories()
+
   return (
     <header className="bg-black text-white">
         <div className="px-2">
@@ -35,13 +40,7 @@ function Header() {
                 </div>
             </div>
             <div className="flex items-center px-3 mb-[1px] bg-gray-800">
-                <Button 
-                    variant={'ghost'}
-                    className="dark header-button flex items-center gap-1 text-base [&_svg]:size-6 "
-                >
-                    <MenuIcon />
-                    All
-                </Button>
+                <Sidebar categories={categories}/>
                 <div className="flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px]">
                     {data.headerMenus.map((menu)=>(
                         <Link href={menu.href}
@@ -56,4 +55,3 @@ function Header() {
   )
 }
 
-export default Header
